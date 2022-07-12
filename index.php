2f557a4f4209ec16ab30c4232b7c1382b072cf54
samkibe <smlkibe87@gmail.com>
2022-07-12 12:14:57 +0300

    Private

--- index.php
+++ index.php
@@ -5,3 +5,4 @@
   echo $name . " is " . $age;
 
 ?>
+// path  || localhost/testpostman/Snppets-in-Building-and-Testing-USSD-and-SMS-Applications/ ||
--- remove.php
+++ remove.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     $text = $_GET['text'];
     $rm = new Remove($text);
@@ -48,54 +47,3 @@
 
 
     ?>
-=======
-<?php
-    $text = $_POST['text'];
-    $rm = new Remove($text);
-
-    //echo $rm->remove99();
-    //echo $rm->remove98();
-
-    echo $rm->remove98($rm->remove99());
-
-    
-    class Remove{
-       var $str;
-       //1*3*5*98*8*1*12*98 => 1*3*8*1  
-
-       //1*3*5*99*8*3 =>8*3
-       function __construct($s)
-       {
-        
-         $this->str = $s;
-
-       }
-       //1st Function
-       public function remove99(){
-        $explodeStr = explode("*",$this->str);
-        while(array_search("99",$explodeStr) != false){
-             $firstIndex = array_search("99",$explodeStr);
-             $explodeStr = array_slice($explodeStr,$firstIndex+1);
-
-        }
-        return join("*",$explodeStr);
-
-       }
-      // 2nd Function
-       public function remove98(){
-        $explodeStr = explode("*",$this->str);
-        while(array_search("98",$explodeStr) != false){
-             $firstIndex = array_search("98",$explodeStr);
-             array_splice($explodeStr,$firstIndex-1,2);
-
-        }
-        return join("*",$explodeStr);
-
-
-
-       }
-    }
-
-
-    ?>
->>>>>>> c3419e3e0925f8fc61e21199ba4f61d003164b72
