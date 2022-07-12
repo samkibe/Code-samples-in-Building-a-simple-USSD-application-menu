307556433d6c724658476d305f403ca9dc10d235
Kibe <smlkibe87@gmail.com>
2022-07-12 16:18:03 +0300

    Update phpversioncode.php
    Works fine when configured in the simulator environment in africaTalking site.

--- phpversioncode.php
+++ phpversioncode.php
@@ -7,23 +7,23 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
+    $response  = "What would you want to check \n";
     $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+    $response .= "2. My Mobile number";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
+    $response = "Choose account information you want to view \n";
     $response .= "1. Account number \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
+    $response = "Your phone number is ".$phoneNumber;
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
+    $accountNumber  = "ACC70303";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
