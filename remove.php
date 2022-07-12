d69ebc3530fcd5715d79165d8f83356ce188c9cf
Kibe <smlkibe87@gmail.com>
2022-07-12 12:03:13 +0300

    Update remove.php

--- remove.php
+++ remove.php
@@ -1,56 +1,6 @@
-<<<<<<< HEAD
+
 <?php
     $text = $_GET['text'];
-    $rm = new Remove($text);
-
-    //echo $rm->remove99();
-    //echo $rm->remove98();
-
-    echo $rm->remove98($rm->remove99());
-
-    
-    class Remove{
-       var $str;
-       //1*3*5*98*8*1*12*98 => 1*3*8*1  
-
-       //1*3*5*99*8*3 =>8*3
-       function __construct($s)
-       {
-        
-         $this->str = $s;
-
-       }
-       
-       public function remove99(){
-        $explodeStr = explode("*",$this->str);
-        while(array_search("99",$explodeStr) != false){
-             $firstIndex = array_search("99",$explodeStr);
-             $explodeStr = array_slice($explodeStr,$firstIndex+1);
-
-        }
-        return join("*",$explodeStr);
-
-       }
-
-       public function remove98(){
-        $explodeStr = explode("*",$this->str);
-        while(array_search("98",$explodeStr) != false){
-             $firstIndex = array_search("98",$explodeStr);
-             array_splice($explodeStr,$firstIndex-1,2);
-
-        }
-        return join("*",$explodeStr);
-
-
-
-       }
-    }
-
-
-    ?>
-=======
-<?php
-    $text = $_POST['text'];
     $rm = new Remove($text);
 
     //echo $rm->remove99();
@@ -81,7 +31,7 @@
         return join("*",$explodeStr);
 
        }
-      // 2nd Function
+       //2nd Function
        public function remove98(){
         $explodeStr = explode("*",$this->str);
         while(array_search("98",$explodeStr) != false){
@@ -98,4 +48,3 @@
 
 
     ?>
->>>>>>> c3419e3e0925f8fc61e21199ba4f61d003164b72
