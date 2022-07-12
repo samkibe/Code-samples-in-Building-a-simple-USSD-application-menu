9ceda34e6db57db5cd7f7584dcc100cbde6ca915
Kibe <smlkibe87@gmail.com>
2022-07-12 16:25:22 +0300

    Update phpversioncode.php

--- phpversioncode.php
+++ phpversioncode.php
@@ -7,19 +7,19 @@ $text        = $_POST["text"];
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "What would you want to check \n";
+    $response  = "CON What would you want to check \n";
     $response .= "1. My Account \n";
     $response .= "2. My Mobile number";
 
 } else if ($text == "1") {
     // Business logic for first level response
-    $response = "Choose account information you want to view \n";
+    $response = "CON Choose account information you want to view \n";
     $response .= "1. Account number \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $response = "Your phone number is ".$phoneNumber;
+    $response = "END Your Mobile number is ".$phoneNumber;
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
