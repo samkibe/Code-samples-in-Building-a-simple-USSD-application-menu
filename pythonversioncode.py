514667b375d1dc54ba2926c60acd5e156a71a9f9
Kibe <smlkibe87@gmail.com>
2022-07-13 12:23:12 +0300

    Update pythonversioncode.py

--- pythonversioncode.py
+++ pythonversioncode.py
@@ -8,14 +8,14 @@ def ussd():
   # Read the variables sent via POST from our API
   session_id   = request.values.get("sessionId", None)
   serviceCode  = request.values.get("serviceCode", None)
-  phone_number = request.values.get("phoneNumber", None)
+  phone_number = request.values.get("mobileNumber", None)
   text         = request.values.get("text", "default")
 
   if text      == '':
       # This is the first request. Note how we start the response with CON
       response  = "CON What would you want to check \n"
       response += "1. My Account \n"
-      response += "2. My phone number"
+      response += "2. My mobile number"
 
   elif text    == '1':
       # Business logic for first level response
@@ -24,7 +24,7 @@ def ussd():
 
   elif text   == '2':
       # This is a terminal request. Note how we start the response with END
-      response = "END Your phone number is " + phone_number
+      response = "END Your mobile number is " + phone_number
 
   elif text          == '1*1':
       # This is a second level response where the user selected 1 in the first instance
@@ -33,7 +33,7 @@ def ussd():
       response       = "END Your account number is " + accountNumber
 
   else :
-      response = "END Invalid choice"
+      response = "END OOPS! Invalid Entry"
 
   # Send the response back to the API
   return response
