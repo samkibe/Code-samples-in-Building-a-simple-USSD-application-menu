e6a93fb15cbcae6d230d4de7625001b83fbbe3a9
Kibe <smlkibe87@gmail.com>
2022-07-13 12:45:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 
     echo "👋 Hi, I am kibe, a researcher, an aspiring Data scientist TO  complement Information security.
 
-    👀 I’m interested in Data Science and Cyber Security."
+    👀 I’m interested in Data Science and Cyber Security.
+    🌱 I’m currently learning 'Building and Testing USSD and SMS Applications,' Data science, Text mining on twitter and anaylsis of said texts through R, Ethical hacking,
+
+       (Data governance and information security).
+
+          💞️ I’m looking to collaborate on data science projects & information security
+
+           📫 How to reach me ... Twitter- @kibe_ish"
 
 
 
